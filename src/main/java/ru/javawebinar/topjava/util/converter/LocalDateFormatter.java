b2e7878644668fe8775287ee612b067715b1bb0d
art-respublica <respublica_news@ukr.net>
2016-10-25 14:43:14 +0300

    HW7 (optional)

---
+++ src/main/java/ru/javawebinar/topjava/util/converter/LocalDateFormatter.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.util.converter;
+
+import org.springframework.format.Formatter;
+import ru.javawebinar.topjava.util.TimeUtil;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+/**
+ * Created by Respublica on 25.10.2016.
+ */
+public class LocalDateFormatter implements Formatter<LocalDate> {
+
+    @Override
+    public LocalDate parse(String text, Locale locale) throws ParseException {
+        return TimeUtil.parseLocalDate(text);
+    }
+
+    @Override
+    public String print(LocalDate lt, Locale locale) {
+        return lt.format(DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/util/converter/LocalTimeFormatter.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.util.converter;
+
+import org.springframework.format.Formatter;
+import ru.javawebinar.topjava.util.TimeUtil;
+
+import java.text.ParseException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+/**
+ * Created by Respublica on 25.10.2016.
+ */
+public class LocalTimeFormatter implements Formatter<LocalTime> {
+
+    @Override
+    public LocalTime parse(String text, Locale locale) throws ParseException {
+        return TimeUtil.parseLocalTime(text);
+    }
+
+    @Override
+    public String print(LocalTime lt, Locale locale) {
+        return lt.format(DateTimeFormatter.ISO_LOCAL_TIME);
+    }
+}
