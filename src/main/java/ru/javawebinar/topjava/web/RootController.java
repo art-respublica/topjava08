8ce5acd36a31c707f9d5e3f3532d9180281ad0ae
art-respublica <respublica_news@ukr.net>
2016-11-11 11:29:34 +0300

    HW9 (have done)

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -8,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.javawebinar.topjava.AuthorizedUser;
-import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.util.MealsUtil;
 
 /**
  * User: gkislin
@@ -18,9 +14,6 @@ import ru.javawebinar.topjava.util.MealsUtil;
  */
 @Controller
 public class RootController {
-
-    @Autowired
-    private MealService mealService;
 
     @GetMapping("/")
     public String root() {
@@ -43,8 +36,6 @@ public class RootController {
 
     @GetMapping("/meals")
     public String meals(Model model) {
-        model.addAttribute("meals",
-                MealsUtil.getWithExceeded(mealService.getAll(AuthorizedUser.id()), AuthorizedUser.getCaloriesPerDay()));
         return "meals";
     }
 }
---
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -0,0 +1,54 @@
+var ajaxUrl = 'ajax/profile/meals/';
+var datatableApi;
+
+function updateTable() {
+    $.get(ajaxUrl, updateTableByData);
+}
+
+$(function () {
+    datatableApi = $('#datatable').DataTable({
+        "ajax": {
+            "url": ajaxUrl,
+            "dataSrc": ""
+        },
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "dateTime",
+                "render": function (date, type, row) {
+                    if (type == 'display') {
+                        return '<span>' + date.substring(0, 10) + '</span>';
+                    }
+                    return date;
+                }
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "defaultContent": "",
+                "orderable": false,
+                "render": renderEditBtn
+            },
+            {
+                "defaultContent": "",
+                "orderable": false,
+                "render": renderDeleteBtn
+            }
+        ],
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ],
+        "createdRow": function (row, data, dataIndex) {
+            $(row).addClass(data.exceed ? 'exceeded' : 'normal');
+        },
+        "initComplete": makeEditable
+    });
+});
